184b6d026bf0d9a07038bfc4bf165fe5c71d78da
evanlv <evan.levaillant@seti-tec.fr>
2022-09-14 16:09:29 +0200

    add new exercice and rename files

---
+++ filterWithReduce.js
@@ -0,0 +1,23 @@
+const arr1 = ["a"];
+const arr2 = ["a", "b"];
+const arr3 = ["a", "b", "c"];
+const arr4 = ["a", "b", "c", "d"];
+const arr5 = ["a", "b"];
+const arr6 = ["", "", ""];
+const arr7 = [];
+
+const filter = (arr, separator) => {
+  console.log(arr.slice(0, -1));
+  arr.reduce((acc, joinedItem) => {
+    const result = acc.concat(...joinedItem, separator);
+    return result;
+  }, "");
+};
+
+console.log(join(arr1, ","));
+console.log(join(arr2, ","));
+// console.log(join(arr3, ""));
+// console.log(join(arr4, "x"));
+// console.log(join(arr5, "COMMA"));
+// console.log(join(arr6, ","));
+// console.log(join(arr7, ","));
