651e9143c23ab71212530d5341f4f5f7e8d018c9
evanlv <evan.levaillant@seti-tec.fr>
2022-09-15 15:53:33 +0200

    chore: clean code

--- filterWithReduce.js
+++ filterWithReduce.js
@@ -1,15 +1,11 @@
 const arr1 = [1, 2, 3];
-const arr2 = [1, 2, 3];
-const arr3 = [1, 2, 3];
-const arr4 = [null, undefined];
-const arr5 = [];
+const arr2 = [null, undefined];
 
-const filter = (arr, callback) => {
-  return arr.reduce((xs, x) => {}, []);
-};
+const filter = (arr, callback) =>
+  callback ? arr.reduce((xs, x) => [...xs, x], []) : [];
 
-console.log(filter([1, 2, 3], (num) => num >= 0));
-console.log(filter([1, 2, 3], (num) => num > 1));
-console.log(filter([1, 2, 3], (num) => num > 5));
-console.log(filter([null, undefined], (num) => true));
+console.log(filter(arr1, (num) => num >= 0));
+console.log(filter(arr1, (num) => num > 1));
+console.log(filter(arr1, (num) => num > 5));
+console.log(filter(arr2, (num) => true));
 console.log(filter([], (num) => true));
