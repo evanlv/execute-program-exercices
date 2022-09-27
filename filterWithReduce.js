64b7d32929fa611c7084e0ae960c1ac3cf52885a
evanlv <evanlvdev@gmail.com>
2022-09-27 22:57:31 +0200

    add filterWithReduce solution

--- filterWithReduce.js
+++ filterWithReduce.js
@@ -1,11 +1,11 @@
-const arr1 = [1, 2, 3];
-const arr2 = [null, undefined];
+const arr1 = [1, 2, 3]
+const arr2 = [null, undefined]
 
 const filter = (arr, callback) =>
-  callback ? arr.reduce((xs, x) => [...xs, x], []) : [];
+  arr.reduce((xs, x) => (callback(x) ? [...xs, x] : []), [])
 
-console.log(filter(arr1, (num) => num >= 0));
-console.log(filter(arr1, (num) => num > 1));
-console.log(filter(arr1, (num) => num > 5));
-console.log(filter(arr2, (num) => true));
-console.log(filter([], (num) => true));
+console.log(filter(arr1, (num) => num >= 0))
+console.log(filter(arr1, (num) => num > 1))
+console.log(filter(arr1, (num) => num > 5))
+console.log(filter(arr2, (num) => true))
+console.log(filter([], (num) => true))
